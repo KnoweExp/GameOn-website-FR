1ba524477431194ba02004c952768e4b8c78658e
SALM Loïc <loic.salm8@gmail.com>
2023-10-11 17:12:29 +0200

    error messages form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,7 +12,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -36,17 +35,30 @@ function closeModal() {
 function validate(event) {
   
  
+  
+  // Si validation réussit, vous pouvez fermer la modal ici
+  const modalbg = document.querySelector(".bground");
+  modalbg.style.display = "none";
+  
+  
+  event.preventDefault();
+  
+  
+  return true;
+}
+
+// Définissez la fonction de validation en dehors du gestionnaire d'événements
+function validate() {
+  // ... (votre code de validation)
+
   // Si validation réussit, vous pouvez fermer la modal ici
   const modalbg = document.querySelector(".bground");
   modalbg.style.display = "none";
 
-  // Supprimez la classe "no-scroll" du corps du document
-  const body = document.querySelector("body");
   
 
   return true;
 }
-
 
 const form = document.querySelector("form[name='reserve']");
 
@@ -65,10 +77,57 @@ form.addEventListener("submit", function (event) {
   emailValue = document.getElementById("email").value;
   quantityValue = document.getElementById("quantity").value;
   locationValue = document.querySelector("input[name='location']:checked");
+  
+  function validateFirstName(firstName) {
+      const errorContainer = document.getElementById("firstNameError");
+    
+      if (firstName.length < 2) {
+        errorContainer.textContent = "Le prénom doit avoir au moins 2 caractères";
+        console.log("erreur : Le prénom doit avoir au moins 2 caractères");
+        return false;
+      }
+    }
+    function validateLastName(lastName) {
+      const errorContainer = document.getElementById("lastNameError");
+    
+      if (lastName.length < 2) {
+        errorContainer.textContent = "Le nom doit avoir au moins 2 caractères";
+        console.log("erreur : Le nom doit avoir au moins 2 caractères");
+        return false;
+      }
+    } 
+    
+    function validateEmail(email) {
+      const errorContainer = document.getElementById("emailError");
+      const emailPattern = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+$/i; 
+      if (!emailPattern.test(email)) {
+          errorContainer.textContent = "Veuillez entrer une adresse email valide";
+          return false;
+      }
+  }
 
-  // Validation des champs
+    function validateForm() {
+      let isValid = true;
+
+      // Vérification de chaque champ
+      if (!validateFirstName(firstNameValue)) isValid = false;
+      if (!validateLastName(lastNameValue)) isValid = false;
+      if (!validateEmail(emailValue)) isValid = false;
+      
+
+      // Si validations réussies, effectuez actions.
+      if (isValid) {
+          const modalbg = document.querySelector(".bground");
+          modalbg.style.display = "none";
+          const body = document.querySelector("body");
+      }
+
+      return isValid;
+  }
+
+
   if (!validateForm()) {
-    event.preventDefault(); // Empêche la soumission du formulaire en cas d'erreur
+      event.preventDefault();
   }
 });
 
@@ -82,26 +141,8 @@ function validateForm() {
   const location = locationValue;
   const terms = document.getElementById("checkbox1").checked;
 
-  // Validation du prénom
-  if (firstName.length < 2) {
-    alert("Le prénom doit avoir au moins 2 caractères.");
-    return false;
-  }
 
-  // Validation du nom de famille
-  if (lastName.length < 2) {
-    alert("Le nom de famille doit avoir au moins 2 caractères.");
-    return false;
-  }
-
-  // Validation de l'adresse électronique (utilisez une expression régulière appropriée)
-  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  if (!emailPattern.test(email)) {
-    alert("L'adresse électronique n'est pas valide.");
-    return false;
-  }
-
-  // Validation de la quantité
+/*   // Validation de la quantité
   if (isNaN(quantity) || quantity < 0) {
     alert("Veuillez entrer une valeur numérique valide pour la quantité.");
     return false;
@@ -117,7 +158,7 @@ function validateForm() {
   if (!terms) {
     alert("Vous devez accepter les conditions générales.");
     return false;
-  }
+  } */
 
   // Si toutes les validations réussissent, vous pouvez fermer la modal ici
   const modalbg = document.querySelector(".bground");
@@ -125,6 +166,7 @@ function validateForm() {
 
   // Réinitialisez la classe "no-scroll" du corps du document
   const body = document.querySelector("body");
+  body.classList.remove("no-scroll");
 
   return true;
 }
